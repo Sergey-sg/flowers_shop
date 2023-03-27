1d9a4b31bcf7999f25e96f1cf5c059267ad89325
Serhey Hlushak <glushak_sergey@i.ua>
2023-03-27 23:20:41 +0300

    worked with banner image position

--- frontend/src/pages/components/BannerFlowers.tsx
+++ frontend/src/pages/components/BannerFlowers.tsx
@@ -7,19 +7,22 @@ const BannerFlowers: React.FC = () => {
     }
 
     return (
-        <div className='container h-2/6 w-full mx-auto mt-4 mb-12 relative static'>
-          <img
-              className='inline-block'
+      <div className='container'>
+        <div className='h-3/6 w-full mx-auto pt-14 px-20 mb-12 relative'>
+          <img 
+              className='absolute top-0 left-0 w-2/4'
+              style={{ maxWidth: '45%'}}
               src={bannerImage}
               alt={'flowers'}
           />
-          <div className='rounded-lg flex w-10/12 m-auto h-4/6' style={divStyle}>
-            <div className='text-slate-50 my-auto mr-24 ml-auto'>
+          <div className='rounded-lg flex m-auto' style={divStyle}>
+            <div className='text-slate-50 my-auto py-28 mr-40 ml-auto'>
                 <p className='font-medium text-4xl leading-10'>Unrivaled choice</p>
                 <span className='font-normal text-xs leading-4'>Feel cared for with selected flowers</span>
             </div>
           </div>
         </div>
+      </div>
     )
 }
 
