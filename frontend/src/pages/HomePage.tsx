d96f382e9e178097175575231b53868cc0ed195d
Serhey Hlushak <glushak_sergey@i.ua>
2023-03-28 18:23:17 +0300

    created FlowersOffers component

--- frontend/src/pages/HomePage.tsx
+++ frontend/src/pages/HomePage.tsx
@@ -1,21 +1,16 @@
 import React from 'react'
 import BannerFlowers from './components/BannerFlowers'
+import FlowersOffers from './components/FlowersOffers'
 import Header from './components/Header'
-
-const FlowersOffers: React.FC = () => {
-    return (
-        <div className='w-10/12 mx-auto'>
-            <div className='text-slate-50'>Offers</div>
-        </div>
-    )
-}
 
 const HomePage: React.FC = () => {
     return (
         <div className='bg-black h-screen min-h-screen h-max'>
             <Header/>
             <BannerFlowers/>
+            <br/>
             <FlowersOffers/>
+            <br/>
         </div>
     )
 }
