247ce64741472b238abae1a91c6c6ada7d4614d9
Serhii Hlushak <glushak_sergey@i.ua>
2023-04-15 16:32:56 +0300

    backend: fixed filters

--- backend/apps/flower/filters.py
+++ backend/apps/flower/filters.py
@@ -1,13 +1,14 @@
 from django_filters import rest_framework as filters
 
+from .models import Product
+
 
 class ProductFilter(filters.FilterSet):
     """filter for Product"""
     ordering = filters.OrderingFilter(
         fields=(('price', 'price'), ('number_of_sold', 'sold'), ),)
     search = filters.CharFilter(field_name='name', lookup_expr='icontains')
-    category = filters.BaseInFilter(
-        field_name='category__slug', lookup_expr='in', method='filter_category')
+    category = filters.BaseInFilter(method='filter_category')
 
     def filter_category(self, queryset, *args):
         """get the filter value by category and return queryset of product"""
